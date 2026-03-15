c9086c16a1707e14aea0846acf025c7069115e4f
Devendra Kumar Narware <devn2405@gmail.com>
2026-03-15 16:30:39 +0530

    first commit

---
+++ 01hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/Devnarware/hello
+
+go 1.26.1
---
+++ 02variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+
+import "fmt"
+
+const hello =  "hello ji uncle"
+
+func main()  {
+
+	fmt.Println("") ;
+	fmt.Println("") ;
+	
+	var username string = "Dev" ;
+	fmt.Printf("variable is type of %T \n" , username) ;
+	
+	
+	isLoggedIN := true ;
+	fmt.Println(isLoggedIN) ;
+
+	hello := "jai ram ji ki"
+
+	
+	fmt.Println(hello)
+	
+	
+	fmt.Println("") ;
+}
