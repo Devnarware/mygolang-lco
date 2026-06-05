e9de436c5aed91e5637473831a3b6971bc359822
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-05 20:45:53 +0530

    Refactor Shape interface and implement area methods for Rectangle and Circle; update notes on structs and methods

--- practice/main.go
+++ practice/main.go
@@ -2,27 +2,44 @@ package main
 
 import "fmt"
 
-type Rectangle struct{
-	width int
-	height int
+
+type Shape interface{
+	area() float64
 }
 
-func (r Rectangle) area() int{
-	return r.height * r.width
-} 
-func (r Rectangle) perim() int{
+type Rectangle struct {
+	width  int
+	height int
+}
+type Circle struct {
+	radius int
+}
+
+func (r Rectangle) area() float64 {
+	return float64(r.height * r.width)
+}
+func (c Circle) area() float64 {
+	return 0.5*(3.14 * float64(c.radius) * float64(c.radius))
+}
+
+func (r Rectangle) perim() int {
 	return 2*(r.height + r.width)
-} 
+}
+
+func printArea(s Shape){
+	fmt.Printf("the area is %.2f \n", s.area())
+}
 
 func main() {
 
-	var a Rectangle 
-	a.height = 10
-	a.width = 20
+	var r Rectangle
+	r.height = 10
+	r.width = 20
 
-	fmt.Println(a.area())
-	fmt.Println(a.perim())
+	var c Circle 
+	c.radius = 2 
+
+	printArea(r)
+	printArea(c)
 
 }
-
-
--- practice/notes.md
+++ practice/notes.md
@@ -15,3 +15,27 @@ m := make(map[key_type] val_type)
 ```
 
 - we can only use for each loop on map
+
+## Struct
+
+strut is just like the blueprint, it doesn't hold any data but help to create it, you can create your own data type using it 
+
+```
+type Box struct{
+    height int
+    width int
+    length int
+}
+```
+
+## method 
+
+if we combine metod and struct it will beacome the class in java or c++, where struct help us definfing the variables, method helps us to perform some opertion using that variables
+
+- methods are just the fucntion with the struct involved means you can perform various operation on the variable of the struct
+
+```
+ func (b Box) area() int{
+    return 2 * (length * width + length * height + height + width)
+ }
+```
