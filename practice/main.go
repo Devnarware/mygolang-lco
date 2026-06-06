a5d7fad0b9e8587ac077abf13e1ec2fdaf69ee0c
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-06 20:35:28 +0530

    Remove Shape interface and related structs; simplify main function to clear and print slice

--- practice/main.go
+++ practice/main.go
@@ -2,44 +2,18 @@ package main
 
 import "fmt"
 
-
-type Shape interface{
-	area() float64
-}
-
-type Rectangle struct {
-	width  int
-	height int
-}
-type Circle struct {
-	radius int
-}
-
-func (r Rectangle) area() float64 {
-	return float64(r.height * r.width)
-}
-func (c Circle) area() float64 {
-	return 0.5*(3.14 * float64(c.radius) * float64(c.radius))
-}
-
-func (r Rectangle) perim() int {
-	return 2*(r.height + r.width)
-}
-
-func printArea(s Shape){
-	fmt.Printf("the area is %.2f \n", s.area())
-}
-
 func main() {
 
-	var r Rectangle
-	r.height = 10
-	r.width = 20
+	arr := []int {1,2,3,4,5,6}
 
-	var c Circle 
-	c.radius = 2 
 
-	printArea(r)
-	printArea(c)
+	clearSlice(&arr)
+	fmt.Println(arr)
 
 }
+
+func clearSlice(arr *[]int){
+	*arr = []int {}
+}
+
+
