dc1e89a98c4c4bcd6b3893c60947d6baf418a982
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-12 22:56:29 +0530

    custom logger

--- practice/cmd/web/main.go
+++ practice/cmd/web/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 type config struct{
 	addr string
 }
+//2. why we are using the struct method when we can save it in a local variable ??
+
+type application struct{
+	looger *slog.Logger
+}
+//4. creating the logger with the help of struct, so that we can use it anywhere in the package
+
 
 func main(){
 
@@ -16,14 +24,25 @@ func main(){
 	flag.StringVar(&cnfg.addr, "addr", ":4000", "HTTP network address")
 	flag.Parse()
 	mux := http.NewServeMux()
+	
+	// it is for the congiguation 
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	//3. created a logger inside the main fuction but we can't use it outside the main func
+
+	
+
 
 	mux.HandleFunc("GET /", home)
 
-	log.Printf("Server is staring at the port %s", cnfg.addr)
+	
+	logger.Info("Server is staring at", "addr", cnfg.addr)
 	err := http.ListenAndServe(cnfg.addr, mux)
 
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(err.Error())
 	}
+
+	os.Exit(1)
 
 }
