7555db444e6afa7afe939971befb4129fb23ae8f
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-13 21:53:35 +0530

    comment added

--- practice/cmd/web/routes.go
+++ practice/cmd/web/routes.go
@@ -10,6 +10,7 @@ func (app *application)routes() *http.ServeMux{
 	mux.HandleFunc("GET /about", app.about)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	// these are the routes we are gonna use 
 
 	return mux
 }
